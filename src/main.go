3deccc7fd19d6d365ce38646fb79d0536efa6a07
Roberts, Benjamin <benjamin.roberts@sky.uk>
2017-11-21 21:44:18 +0000

    Make a heckin simple solution using repeated addition of each desks divisors

--- src/main.go
+++ src/main.go
@@ -5,9 +5,37 @@ import (
 	"time"
 )
 
+func calculatePresents(desk int) int {
+	total := 10 * desk
+	for i := desk-1; i > 0; i-- {
+		if desk % i == 0 {
+			total += i * 10
+		}
+	}
+
+	return total
+}
+
+
 func main() {
 	start := time.Now()
 	fmt.Printf("Hello, world\n")
+
+	target := 50000000
+	currentMax := 0
+	var desk int
+
+	for i := 1; currentMax < target; i++ {
+		presentsForDesk := calculatePresents(i)
+		fmt.Printf("%v: %v\n", i, presentsForDesk)
+		if presentsForDesk > currentMax {
+			currentMax = presentsForDesk
+			desk = i
+		}
+	}
+
+
 	execTime := time.Since(start)
+	fmt.Printf("%v\n", desk)
 	fmt.Printf("%v\n", execTime)
 }
