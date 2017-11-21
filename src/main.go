fb25efd90e387a46766749025fa0425ebe36323c
Roberts, Benjamin <benjamin.roberts@sky.uk>
2017-11-21 23:27:05 +0000

    Optimise the var assignments a bit

--- src/main.go
+++ src/main.go
@@ -5,9 +5,10 @@ import (
 	"time"
 )
 
-func calculatePresents(desk int) int {
-	total := 10 * desk
-	for i := desk-1; i > 0; i-- {
+func calculatePresents(desk uint32) uint32 {
+	var total, i uint32
+	total = 10 * desk
+	for i = desk-1; i > 0; i-- {
 		if desk % i == 0 {
 			total += i * 10
 		}
@@ -18,15 +19,17 @@ func calculatePresents(desk int) int {
 
 
 func main() {
-	start := time.Now()
-	fmt.Printf("Hello, world\n")
+	var desk, target, currentMax, presentsForDesk, i uint32
+	var start time.Time
+	var duration time.Duration
 
-	target := 50000000
-	currentMax := 0
-	var desk int
+	target = 50000000
+	currentMax = 0
 
-	for i := 0; currentMax < target; i+=12 {
-		presentsForDesk := calculatePresents(i)
+	start = time.Now()
+
+	for i = 0; currentMax < target; i+=12 {
+		presentsForDesk = calculatePresents(i)
 		if presentsForDesk > currentMax {
 			fmt.Printf("%v: %v\n", i, presentsForDesk)
 			currentMax = presentsForDesk
@@ -34,8 +37,8 @@ func main() {
 		}
 	}
 
+	duration = time.Since(start)
 
-	execTime := time.Since(start)
 	fmt.Printf("%v\n", desk)
-	fmt.Printf("%v\n", execTime)
+	fmt.Printf("%v\n", duration)
 }
