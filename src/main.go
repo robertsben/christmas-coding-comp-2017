8dbfb248176e000a32ba7f5715e8cd80a098f6bc
Roberts, Benjamin <benjamin.roberts@sky.uk>
2017-11-26 02:16:43 +0000

    Implement Atkin sieve for finding primes up to square root of present limit

--- src/main.go
+++ src/main.go
@@ -5,10 +5,57 @@ import (
 	"time"
 	"os"
 	"strconv"
+	"math"
 )
 
 type DeskData struct {
 	desk, presents uint32
+}
+
+var limit uint32
+
+// here we assume we only need primes up to 65535; i.e. we'll deal only with numbers <= 4294836225 (almost all unsigned 32 bit integers)
+var primes []uint32
+
+func atkinSieve(max uint32) {
+	var x, y, n uint32
+	maxSqrt := math.Sqrt(float64(max))
+
+	isPrime := make([]bool, max, max)
+
+	for x = 1; float64(x) <= maxSqrt; x++ {
+		for y = 1; float64(y) <= maxSqrt; y++ {
+			n = (4*x*x) + (y*y)
+			if n <= max && (n % 12 == 1 || n %12 == 5) {
+				isPrime[n] = !isPrime[n]
+			}
+			n = (3*x*x) + (y*y)
+			if n <= max && n % 12 == 7 {
+				isPrime[n] = !isPrime[n]
+			}
+			n = (3*x*x) - (y*y)
+			if x > y && n <= max && n % 12 == 11 {
+				isPrime[n] = !isPrime[n]
+			}
+		}
+	}
+
+	for n = 5; float64(n) <= maxSqrt; n++ {
+		if isPrime[n] {
+			for y = n*n; y < max; y += n*n {
+				isPrime[y] = false
+			}
+		}
+	}
+
+	isPrime[2] = true
+	isPrime[3] = true
+
+	for x = 0; int(x) < len(isPrime)-1; x++ {
+		if isPrime[x] {
+			primes = append(primes, x)
+		}
+	}
 }
 
 func calculatePresents(desk uint32) uint32 {
@@ -38,33 +85,33 @@ func deskDelivery(deskDataChannel chan DeskData, quitChannel chan bool) {
 
 
 func main() {
-	var desk, target, currentMax uint32
+	var desk, target, primeTarget uint32
 	var start time.Time
 	var duration time.Duration
-	var deskData DeskData
-	deskDataChannel := make(chan DeskData)
-	quitChannel := make(chan bool)
+	//var deskData DeskData
+	//deskDataChannel := make(chan DeskData)
+	//quitChannel := make(chan bool)
 
 	parsedTarget, _ := strconv.ParseInt(os.Getenv("PRESENTS"), 10, 64)
 	target = uint32(parsedTarget)
-	currentMax = 0
+	primeTarget = uint32(math.Sqrt(float64(target)))
 
 	start = time.Now()
 
-	go func() {
-		for deskData = range deskDataChannel {
-			if deskData.presents > currentMax {
-				fmt.Printf("%v: %v\n", deskData.desk, deskData.presents)
-				currentMax = deskData.presents
-				desk = deskData.desk
-			}
-			if deskData.presents > target {
-				quitChannel <- true
-			}
-		}
-	}()
-
-	deskDelivery(deskDataChannel, quitChannel)
+	//go func() {
+	//	for deskData = range deskDataChannel {
+	//		if deskData.presents > currentMax {
+	//			fmt.Printf("%v: %v\n", deskData.desk, deskData.presents)
+	//			currentMax = deskData.presents
+	//			desk = deskData.desk
+	//		}
+	//		if deskData.presents > target {
+	//			quitChannel <- true
+	//		}
+	//	}
+	//}()
+	//
+	//deskDelivery(deskDataChannel, quitChannel)
 
 	//for i = 0; currentMax < target; i+=12 {
 	//	presentsForDesk = calculatePresents(i)
@@ -75,7 +122,12 @@ func main() {
 	//	}
 	//}
 
+	atkinSieve(primeTarget)
+
+
 	duration = time.Since(start)
+
+	fmt.Printf("%v\n", primes)
 
 	fmt.Printf("%v\n", desk)
 	fmt.Printf("%v\n", duration)
