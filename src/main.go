2371e3fe091a0fa676425228107af1d1e2788346
Roberts, Benjamin <benjamin.roberts@sky.uk>
2017-11-27 18:48:18 +0000

    write some data to file so we can use it for tests later on

--- src/main.go
+++ src/main.go
@@ -5,7 +5,8 @@ import (
 	"time"
 	"os"
 	"strconv"
-	"math"
+	//"math"
+	"log"
 )
 
 func calculatePresents(desk uint32) uint32 {
@@ -23,9 +24,15 @@ func calculatePresents(desk uint32) uint32 {
 
 func main() {
 	var desk, target, currentMax, presentsForDesk, i uint32
-	var deskIsMoreThanSqrtOfPresents bool
+	//var deskIsMoreThanSqrtOfPresents bool
 	var start time.Time
 	var duration time.Duration
+
+	f, err := os.OpenFile("result-data.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
 
 	parsedTarget, _ := strconv.ParseInt(os.Getenv("PRESENTS"), 10, 64)
 	target = uint32(parsedTarget)
@@ -35,11 +42,12 @@ func main() {
 	fmt.Printf("%v!\n", target)
 	for i = 1; currentMax < target; i+=1 {
 		presentsForDesk = calculatePresents(i)
-		deskIsMoreThanSqrtOfPresents = i >= uint32(math.Sqrt(float64(presentsForDesk)))
-		fmt.Printf("%v: %v: %v\n", i, presentsForDesk, deskIsMoreThanSqrtOfPresents)
+		//deskIsMoreThanSqrtOfPresents = i >= uint32(math.Sqrt(float64(presentsForDesk)))
+		//fmt.Printf("%v: %v: %v\n", i, presentsForDesk, deskIsMoreThanSqrtOfPresents)
 		if presentsForDesk > currentMax {
 			currentMax = presentsForDesk
 			desk = i
+			fmt.Fprintf(f, "%v: %v\n", desk, presentsForDesk)
 		}
 	}
 
