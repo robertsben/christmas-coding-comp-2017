9fa598898a4ac1ffbff333cf0cd03f0c2817ba37
Roberts, Benjamin <benjamin.roberts@sky.uk>
2017-12-08 20:12:20 +0000

    this turned out to be way faster so, fuck it

--- src/main.go
+++ src/main.go
@@ -8,36 +8,37 @@ import (
 )
 
 func main() {
-	//var desk, primeTarget, currentMax, presentsForDesk, step uint32
-	var desk, j, searchLimit, limit uint32
-	var divisor uint8
+	var desk, elf, searchLimit, limit uint32
 	var start time.Time
 	var duration time.Duration
 
 	parsedTarget, _ := strconv.ParseInt(os.Getenv("PRESENTS"), 10, 64)
+	limit = uint32(parsedTarget)
 
 	// start timing now we have all the information
 	start = time.Now()
-	limit = uint32(parsedTarget)
 
-	divisor = 10
-
+	// figure out how small we can make our search range, based on patterns observed from spitting out results
 	if limit >= 390 && limit < 20160 {
-		divisor = 20
+		searchLimit = limit/20
 	} else if limit >= 20160 && limit <= 6770400 {
-		divisor = 30
+		searchLimit = limit/30
 	} else if limit > 6770400 {
-		divisor = 40
+		searchLimit = limit/40
+	} else {
+		searchLimit = limit/10
 	}
 
-	searchLimit = limit/uint32(divisor)
-
+	// make a cache of desks, in which we'll store the quantity of presents
 	cache := make([]uint32, searchLimit+1, searchLimit+1)
 
+	// iterate desks up to the search limit
 	for desk = 1; desk <= searchLimit; desk++ {
-		for j = desk; j <= searchLimit; j += desk {
-			cache[j] += desk
+		// let each elf do his/her desk drop offs
+		for elf = desk; elf <= searchLimit; elf += desk {
+			cache[elf] += desk
 		}
+		// bail as soon as we find the desk with more than PRESENTS
 		if cache[desk] * 10 > limit {
 			break
 		}
