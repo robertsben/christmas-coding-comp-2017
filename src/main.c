f3e1bce911423170fd6c9cc70a4ed9641a206ea5
Roberts, Benjamin <benjamin.roberts@sky.uk>
2017-12-09 11:20:01 +0000

    Improve the creation of making array to be a malloc (well, calloc) so we don't get seg fault for arrays bigger than heap size

--- src/main.c
+++ src/main.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <time.h>
 
+int search_limit, desk, elf, *cache;
+clock_t start, end;
+double cpu_time_used;
 
 int main()
 {
-    int search_limit, desk, elf;
     const int presents = atoi(getenv("PRESENTS"));
-
-    clock_t start, end;
-    double cpu_time_used;
 
     start = clock();
 
@@ -18,21 +16,23 @@ int main()
         search_limit = presents/20;
     } else if (presents >= 20160 && presents <= 6770400) {
         search_limit = presents/30;
-    } else if (presents > 6770400 ) {
+    } else if (presents > 6770400) {
         search_limit = presents/40;
     } else {
         search_limit = presents/10;
     }
 
-    int cache[search_limit+1];
-    memset(cache, 0, sizeof cache);
+    cache = (int*) calloc((search_limit+1), sizeof(int));
+    if (cache == NULL) {
+        printf("Got messed up trying to allocate memory, bailing :'( ");
+        exit(0);
+    }
 
     for (desk = 1; desk <= search_limit; ++desk) {
         for (elf = desk; elf <= search_limit; elf += desk) {
             cache[elf] += desk;
         }
         if (cache[desk] * 10 >= presents) {
-            printf("%d\n", desk);
             break;
         }
     }
@@ -40,6 +40,8 @@ int main()
     end = clock();
     cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
 
+    printf("%d\n", desk);
     printf("%fs\n", cpu_time_used);
+    free(cache);
     return 0;
 }
